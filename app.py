c170a9d65581eba64aa6c779d175752a32891fe3
Roland <roland.bird@gmail.com>
2025-11-20 16:18:09 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, redirect, render_template_string
+from flask import Flask, redirect, render_template_string, session
 from authlib.integrations.flask_client import OAuth
 import secrets
 
